c02db96619d7673325b488911179d22f9e0877b2
countto10 <s10227123@cycu.org.tw>
2017-08-20 12:37:59 +0800

    Edited comment

--- NetFlowData.py
+++ NetFlowData.py
@@ -12,10 +12,10 @@ def Csv2DictList(filePath):
     fieldName = ['Date flow start','Duration','Proto','Src IP Addr','Src Port',
                  'Dst IP Addr','Dst Port','Packets','Bytes','Flow']
     dataList = []
+    sourceFile.readline() # Read the field name line
 
-    sourceFile.readline()
-    lineNum = 0
     for line in sourceFile:
+
         # End of Data
         if line.find('Summary') != -1:
             break
@@ -25,11 +25,13 @@ def Csv2DictList(filePath):
         line = line.replace(" K", "K")
         line = line[:-1]
         data = re.split(r" +-> +| {1,}|:|,+",line[24:])
-
         # print len(data)
 
+        # The row of data is not complete
         if len(data) < 10:
-            print "File: " + filePath + ", At line: " + str(lineNum) + ", the field num in row is: " + str(len(data))
+            print "File: " + filePath +          \
+                  ", At line: " + str(lineNum) + \
+                  ", the field num in row is: " + str(len(data))
             break
 
         data[0] = line[:23]
@@ -37,7 +39,7 @@ def Csv2DictList(filePath):
             data[j] = data[j].replace("M", " M")
             data[j] = data[j].replace("K", " K")
 
-        # Convert data to dictionary type
+        # Put every column into dict with (k:v) = (fieldName:data)
         i = 0
         row = {}
         for name in fieldName:
@@ -47,16 +49,16 @@ def Csv2DictList(filePath):
             else:
                 row[name] = data[i]
             i += 1
-
         # print row
+        
         # Append to dataList
         dataList.append(row)
-    
-    # print dataList[6]
+
     return dataList
 
-# pick port number is 443 or 8080 
-# seperate from src and dst
+# Pick port number == 443 or 8080 
+# Seperate from src and dst
+# Return a summary list of 2 list
 def ReduceByPort(dataList):
     srcList = []
     dstList = []
@@ -66,13 +68,13 @@ def ReduceByPort(dataList):
             srcList.append(data)
         elif (data.get('Dst Port') == '443') | (data.get('Dst Port') == '8080'):
             dstList.append(data)
-    srcList = sorted(srcList, key=lambda k: k['Src IP Addr'])
-    dstList = sorted(dstList, key=lambda k: k['Dst IP Addr'])
+    srcList = sorted(srcList, key=lambda k: k['Src IP Addr']) # Sort by IP address
+    dstList = sorted(dstList, key=lambda k: k['Dst IP Addr']) # Sort by IP address
     serverList.append(srcList)
     serverList.append(dstList)
     return serverList
 
-# create dict for each IP in serverList
+# Create dict for each IP in serverList
 def SeparateByIP(serverList, type):
     serverDict = {}
     newDictList = []
@@ -81,21 +83,24 @@ def SeparateByIP(serverList, type):
     while i < len(serverList):
         if serverList[i].get(type) == selectIP:
             newDictList.append(serverList[i])
+            # The last IP is the same with privious IP
+            # 
             if i == (len(serverList)-1):
                 serverDict.update({selectIP:newDictList})
-        else:                                              # a different IP
+        else: # A different IP
             serverDict.update({selectIP:newDictList})
             newDictList = []
             selectIP = serverList[i].get(type)
             newDictList.append(serverList[i])
-            if i == (len(serverList)-1):                # the last IP different with privious IP
-                serverDict.update({selectIP:newDictList})  # put it into serverDict directly
-                # print "the last IP different with privious IP"
+            # The last IP different with privious IP
+            # Put it into 'serverDict' directly
+            if i == (len(serverList)-1): 
+                serverDict.update({selectIP:newDictList})
         i += 1
 
-    cnt = 0
-    for k in serverDict.keys():
-        cnt += len(serverDict.get(k))
+    # cnt = 0
+    # for k in serverDict.keys():
+    #     cnt += len(serverDict.get(k))
     # print ("cnt: " + str(cnt))
     # print ("len: " + str(len(serverList)))
     return serverDict
@@ -130,8 +135,36 @@ def SplitByHour(serverDict, startStamp):
         newDict.update({s:timeDict})
         print "====== END OF " + s + " =======\n\n"
     return newDict
+dataList = Csv2DictList('spe.csv')
+serverList = ReduceByPort(dataList)
+srcDict = SeparateByIP(serverList[0], 'Src IP Addr')
+dstDict = SeparateByIP(serverList[1], 'Dst IP Addr')
+
+# List of srcDict
+# List of dstDict
+
+# Merge srcDictList, dstDictList with IP
+
+startStamp = datetime.datetime.strptime("2017-03-01 00:00:00.000", "%Y-%m-%d %H:%M:%S.%f")
+srcDict = SplitByHour(srcDict, startStamp)
+dstDict = SplitByHour(dstDict, startStamp)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" Testing Area
 dataList = Csv2DictList('spe.csv')
 # for data in dataList:
 #     print data
@@ -185,40 +218,4 @@ dstDict = SplitByHour(dstDict, startStamp)
 #         for row in dstDict.get(s).get(k):
 #             print row
 #         print "-------\n\n"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 """
-sortedList = []
-for s in serverList:
-    for l in s:
-        sortedList.append(l)
-sortedList = sorted(sortedList, key=lambda k: k['Date flow start'])
-for s in serverList:
-    for l in s:
-        print (l)
-"""
-# for s in serverList:
-#     for data in s:
-#         print data
-
-
-# d1 = datetime.datetime.strptime("2017-02-28 23:54:50.750", "%Y-%m-%d %H:%M:%S.%f")
-# d2 = datetime.datetime.strptime("2017-03-01 00:54:50.750", "%Y-%m-%d %H:%M:%S.%f")
-# if (d2 - d1) >= timedelta(hours = 1):
-#     print d2 - d1
