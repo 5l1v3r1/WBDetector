05e8e5230b9121b7ff1717428a963457f2473e31
wmlab <wmlabwmlab@outlook.com>
2017-09-15 17:18:35 +0800

    isot store in text file

--- pcap2csv.py
+++ pcap2csv.py
@@ -1,12 +1,12 @@
 import os
 
-count = 0
 filePath = "/home/wmlab/Desktop/ISOT_Botnet_DataSet_2010/splited/"
 for root, dirs, files in os.walk(filePath):
     print root
+    count = 0
     for name in files:
         path = os.path.join(root, name)
-        savePath = "/home/wmlab/Desktop/ISOT_Botnet_DataSet_2010/isotCSV/" + name + ".csv"
+        savePath = "/home/wmlab/Desktop/ISOT_Botnet_DataSet_2010/isotCSV/" + name + ".txt"
         cmd = "tshark -r " + path + " -T fields -e frame.time -e _ws.col.Protocol -e ip.src -e ip.dst -e tcp.port -e frame.len > " + savePath
         os.system(cmd)
         count += 1
