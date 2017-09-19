b41c1b948a3fea0c864b13e98422af6af1ff8d5c
qualitywow <qualitygermany13@gmail.com>
2017-09-19 14:51:56 +0800

    Move ExcuteLog to parent directory

---
+++ MaliciousIP.md
@@ -0,0 +1,58 @@
+## From ISOT Dataset Overview  
+https://www.uvic.ca/engineering/ece/isot/assets/docs/isot-datase.pdf  
+
+IP Address      Type of Traffic Generated       Label of Malicious Traffic  
+172.16.2.11     Malicious/ UDP (Storm)          Src/Dst MAC BB:BB:BB:BB:BB:BB  
+172.16.0.2      Malicious/ SMTP Spam(Waledac)   Src/Dst MAC AA:AA:AA:AA:AA:AA  
+172.16.0.11     Malicious/ SMTP Spam(Waledac)   Src/Dst MAC AA:AA:AA:AA:AA:AA  
+172.16.0.12     Malicious/ SMTP Spam (Storm)    Src/Dst MAC AA:AA:AA:AA:AA:AA  
+172.16.2.2      Non-Malicious Normal            Src/Dst MAC  
+172.16.2.3      Non-Malicious Normal            Src/Dst MAC  
+172.16.2.11     Non-Malicious Normal            Src/Dst MAC  
+172.16.2.12     Non-Malicious Normal            Src/Dst MAC  
+172.16.2.12     Malicious/ Zeus                 Src/Dst MAC CC:CC:CC:CC:CC:CC  
+172.16.2.12     Malicious/ Zeus (C & C)         Src/Dst MAC CC:CC:CC:DD:DD:DD  
+172.16.2.13     Non-Malicious Normal            Src/Dst MAC  
+172.16.2.14     Non-Malicious Normal            Src/Dst MAC  
+172.16.2.111    Non-Malicious Normal            Src/Dst MAC  
+172.16.2.112    Non-Malicious Normal            Src/Dst MAC  
+172.16.2.113    Non-Malicious Normal            Src/Dst MAC  
+172.16.2.114    Non-Malicious Normal            Src/Dst MAC  
+
+
+## From UNB  
+http://www.unb.ca/cic/research/datasets/botnet.html  
+
+List of malicious IPs  
+
+IRC  
+192.168.2.112 -> 131.202.243.84  
+192.168.5.122 -> 198.164.30.2  
+192.168.2.110 -> 192.168.5.122  
+192.168.4.118 -> 192.168.5.122  
+192.168.2.113 -> 192.168.5.122  
+192.168.1.103 -> 192.168.5.122  
+192.168.4.120 -> 192.168.5.122  
+192.168.2.112 -> 192.168.2.110  
+192.168.2.112 -> 192.168.4.120  
+192.168.2.112 -> 192.168.1.103  
+192.168.2.112 -> 192.168.2.113  
+192.168.2.112 -> 192.168.4.118  
+192.168.2.112 -> 192.168.2.109  
+192.168.2.112 -> 192.168.2.105  
+192.168.1.105 -> 192.168.5.122  
+Neris: 147.32.84.180  
+RBot: 147.32.84.170  
+Menti: 147.32.84.150  
+Sogou: 147.32.84.140  
+Murlo: 147.32.84.130  
+Virut: 147.32.84.160  
+IRCbot and black hole1: 10.0.2.15  
+Black hole 2: 192.168.106.141  
+Black hole 3: 192.168.106.131  
+TBot: 172.16.253.130, 172.16.253.131, 172.16.253.129, 172.16.253.240  
+Weasel: Botmaster IP: 74.78.117.238; Bot IP: 158.65.110.24  
+Zeus (zeus sample 1 and 2 and 3, bin_zeus): 192.168.3.35, 192.168.3.25, 192.168.3.65, 172.29.0.116  
+Osx_trojan: 172.29.0.109  
+Zero access (zero access 1 and 2): 172.16.253.132, 192.168.248.165  
+Smoke bot: 10.37.130.4  
--- ProcessISOT.py
+++ ProcessISOT.py
@@ -6,6 +6,8 @@ import ast
 import datetime
 from datetime import timedelta
 
+# Take all the data in ISOT
+# And use malicious IP to calculate threshold
 def Text2DataList(filePath):
     fieldName = ['Date flow start', 'Proto', 'Src IP Addr', 'Dst IP Addr', 'Src Port', 'Dst Port', 'Bytes']
 
@@ -46,7 +48,6 @@ def ReduceByPort(dataList):
 
     return serverList
 
-
 def SeparateByIP(serverList):
     serverDict = {}
     newDictList = []
@@ -77,20 +78,155 @@ def SeparateByIP(serverList):
     # print ("len: " + str(len(serverList)))
     return serverDict
 
+def PartI(filePath, tempPath):
+    if not os.path.exists(tempPath):
+        os.mkdir(tempPath)
+
+    for root, dirs, files in os.walk(filePath):
+
+        count = 0
+        for name in files:
+            count += 1
+            filePath = os.path.join(root, name)
+
+            dataList = []
+            dataList = Text2DataList(filePath)
+            serverList = ReduceByPort(dataList)
+
+            if len(serverList) > 0:
+                serverDict = SeparateByIP(serverList)
+
+            for k in serverDict:
+                outFile = open(tempPath + '\\' + k + ".txt", 'a')
+                for row in serverDict[k]:
+                    outFile.write(str(row) + "\n")
+
+            print "Process Rate: " + str(count) + " / " + str(len(files))
+
+def ReadServerFile(path):
+    dataList = []
+    try:
+        file = open(path, 'r')
+        for line in file:
+            dataList.append(eval(line))
+    except Exception as e:
+        print e
+
+
+    return dataList
+
+def Time2Interval(time, startStamp):
+    t = (time - startStamp).days, (time - startStamp).seconds // 3600
+    return t[0] * 24 + t[1] # t[0] days, t[1] hours
+
+def SplitByHour(dataList, startStamp, endStamp):
+    tempList = []           # Data will be stored in 'tempList' hour by hour
+    timeDict = {}           # Create a dict to store data with {timeInterval:tempList}
+
+    for row in dataList:
+        if row.get('Date flow start') < startStamp:
+            pass
+        elif row.get('Date flow start') >= endStamp:
+            pass
+        else:
+            timeInterval = Time2Interval(row.get('Date flow start'), startStamp)
+            if timeInterval in timeDict:
+                tempList = timeDict.get(timeInterval)
+            else:
+                tempList = []
+
+            tempList.append(row)
+            timeDict.update({timeInterval:tempList})
+    return timeDict
+
+def GetMaxSize(timeDict):
+    maxSize = {}
+    for t in timeDict:
+        maxByte = 0.0
+        for row in timeDict[t]:
+            size = row.get('Bytes')
+            if size.endswith('K'):
+                size = float(size.replace('K', '')) * 1024
+                row.update({'Bytes':size})
+            elif size.endswith('M'):
+                size = float(size.replace('M', '')) * 1024 * 1024
+                row.update({'Bytes':size})
+
+            if float(row.get('Bytes')) > maxByte:
+                maxByte = float(row.get('Bytes'))
+        maxSize.update({t:maxByte})
+    return maxSize
+
+def GetHostGroup(timeDict):
+    hostGroup = {}
+    for t in timeDict:
+        hosts = []
+        for row in timeDict[t]:
+            hosts.append(row.get('Dst IP Addr'))
+        hosts = set(hosts)
+        hostGroup.update({t:hosts})
+    return hostGroup
+
+def ExtractFactor(path, startStamp, endStamp, savePath):
+    dataList = []
+    timeDict = {}
+
+    maxSize = {}   # k: timeInterval, v: byte
+    hostGroup = {} # k: timeInterval, v: set(hostIP)
+
+    dataList = ReadServerFile(path)
+    timeDict = SplitByHour(dataList, startStamp, endStamp)
+    maxSize = GetMaxSize(timeDict)
+    hostGroup = GetHostGroup(timeDict)
+
+    i = 1
+    n = 48
+    thr_sigma = 0
+    acs_sigma = 0
+    pss_sigma = 0
+
+    while i < 48:
+        if hostGroup.get(i-1) == None:
+            hostGroup.update({i-1:[]})
+        if hostGroup.get(i) == None:
+            hostGroup.update({i:[]})
+        if maxSize.get(i) == None:
+            maxSize.update({i:0})
+
+        if len(set(hostGroup.get(i))) > 0:
+            thr_sigma += float(len(set(hostGroup.get(i-1)) & set(hostGroup.get(i)))) / len(set(hostGroup.get(i)))
+        acs_sigma += len(set(hostGroup.get(i)))
+        pss_sigma += maxSize.get(i)
+
+        i += 1
+
+    THR = thr_sigma / n
+    AC = acs_sigma
+    if (AC == 0) & (pss_sigma == 0):
+        PSS = 0
+    else:
+        PSS = pss_sigma / AC
+
+    result = [path[19:-4], str(THR), str(AC), str(PSS)]
+    with open(savePath, 'ab') as f:
+        writer = csv.writer(f)
+        writer.writerow(result)
+
+def PartII(tempPath, startStamp, endStamp, savePath):
+    count = 0
+    for root, dirs, files in os.walk(tempPath):
+        for name in files:
+            path = os.path.join(root, name)
+            ExtractFactor(path, startStamp, endStamp, savePath)
+            count += 1
+            print "Process Rate: " + str(count) + " / " + str(len(files))
 
 if __name__ == '__main__':
 
-    # filePath = "D:\\Botnet\\isotCSV"
-    tempPath = "D:\\Botnet\\tempISOT"
+    filePath = "D:\\Botnet\\isotCSV"
+    tempPath = "D:\\Botnet\\TempISOT"
+    savePath = 'D:\\Botnet\\WBDetector\\FactorRecord_Mar06_07.csv'
+    savePath = 'D:\\Botnet\\WBDetector\\SuspiciousIP.csv'
 
-
-    filePath = "D:\\Botnet\\WBDetector\\TestTXT\\newfiles1100.txt"
-    dataList = Text2DataList(filePath)
-    serverList = ReduceByPort(dataList)
-    if len(serverList) > 0:
-        serverDict = SeparateByIP(serverList)
-
-        for k in serverDict:
-            outFile = open(tempPath + "\\" + k + ".txt", 'a')
-            for row in serverDict[k]:
-                outFile.write(str(row) + "\n")
+    # PartI(filePath, tempPath)
+    PartII(tempPath, startStamp, endStamp, savePath)
