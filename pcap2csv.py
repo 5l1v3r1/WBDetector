95432b6227fac4346023f5eabc1ec909388f3328
qualitywow <qualitygermany13@gmail.com>
2017-10-06 14:51:31 +0800

    Update pcap2csv.py

--- pcap2csv.py
+++ pcap2csv.py
@@ -1,12 +1,17 @@
 import os
 
-filePath = "/home/wmlab/Desktop/ISOT_Botnet_DataSet_2010/splited/"
-for root, dirs, files in os.walk(filePath):
+fileDir = "/home/wmlab/Desktop/Botnet_Dataset/Sality/splited/"
+saveDir = "/home/wmlab/Desktop/Botnet_Dataset/Sality/extract/"
+
+if not os.path.exists(saveDir):
+	os.mkdir(saveDir)
+
+for root, dirs, files in os.walk(fileDir):
     print root
     count = 0
     for name in files:
         path = os.path.join(root, name)
-        savePath = "/home/wmlab/Desktop/ISOT_Botnet_DataSet_2010/isotCSV/" + name + ".txt"
+        savePath = saveDir + name + ".txt"
         cmd = "tshark -r " + path + " -T fields -e frame.time -e _ws.col.Protocol -e ip.src -e ip.dst -e tcp.port -e frame.len > " + savePath
         os.system(cmd)
         count += 1
